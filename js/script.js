946b6a5d984e9dfe9b3550ec182d77d6f22cc43b
Michał Markiewicz <michalmarkiewicz0953@gmail.com>
2022-06-03 14:19:16 +0200

    Refactoring, OrderTaker, Customer, UI

--- js/script.js
+++ js/script.js
@@ -6,37 +6,159 @@ startStopButton.addEventListener('click', userInput);
 const cookingTimeInput = document.getElementById('cooking-time-input');
 const customerArrivalTimeInput = document.getElementById('customer-arrival-time-input');
 
-class App
+class UI 
+{
+    printInfo()
+    {
+        const orderAreaQueue = document.getElementById('order-area-queue');
+        setInterval(() => {
+            orderAreaQueue.textContent = "in queue: " + orderTaker.queue.length;
+        }, 0)
+    }
+}
+const ui = new UI();
+
+class Simulation
 {
     constructor()
     {
         this.on = false;
         this.cookingTime = null;
         this.customerArrivalTime = null;
+        this.customers = [];
+    }
+
+    start()
+    {
+        this.on = true;
+        this.customerArrivalLoop();
+        this.customerTaskLoop();
+        orderTaker.takeOrders();
+        ui.printInfo();
+    }
+
+    stop()
+    {
+        this.on = false;
+    }
+
+    customerArrivalLoop()
+    {
+        const loop = setInterval(() => {
+            this.on ? this.customers.push(new Customer) : clearInterval(loop);
+        }, this.customerArrivalTime)
+    }
+
+    customerTaskLoop()
+    {
+        const loop = setInterval(() => {
+            if (this.on)
+            {
+                this.customers.forEach((customer) => {
+                    switch (customer.taskPhase)
+                    {
+                        case "Not started":
+                            customer.completeTask();
+                            break;
+                        case "In progress":
+                            break; 
+                        case "Completed":
+                            break;
+                    }
+                })   
+            }
+            else 
+            {
+                clearInterval(loop);
+            }
+        }, 5)
     }
 }
 
-const app = new App();
+const simulation = new Simulation();
+
+class Customer 
+{
+    constructor()
+    {
+        this.task = "Order";
+        this.taskPhase = "Not started";
+    }
+
+    completeTask()
+    {
+        switch (this.task)
+        {
+            case "Order":
+                this.order();
+                this.taskPhase = "In progress";
+                break;
+            case "Pickup":
+                break;
+            case "Dining":
+                break;
+        }
+    }
+
+    order()
+    {
+        orderTaker.queue.push(this);
+    }
+}
+
+class OrderTaker
+{
+    constructor()
+    {
+        this.queue = [];
+        this.newOrderNumber = 0;
+    }
+
+    takeOrders()
+    {
+        const orderAreaNewOrder = document.getElementById('order-area-new-order');
+
+        const loop = setInterval(() => {
+                if (simulation.on)
+                {
+                            if (this.queue.length > 0)
+                            {
+                                this.queue[0].taskPhase = "Completed";
+                                this.queue.shift();
+                                this.newOrderNumber++;
+                                orderAreaNewOrder.textContent = `new order: #${this.newOrderNumber}`;
+                            }
+                }
+                else 
+                {
+                    clearInterval(loop);
+                }
+            }, 5000)
+    }
+}
+
+const orderTaker = new OrderTaker();
 
 function userInput()
 {
-    if (app.on === true)
+    if (simulation.on === true)
     {
-        app.on = false;
+        simulation.stop()
         startStopButton.textContent = "Start";
     }
-    else if (app.on === false)
+    else if (simulation.on === false)
     {
         const validation = checkUserInput();
+
         if (validation !== "no error")
         {
             return alert(validation);
         }
 
-        app.on = true;
-        app.cookingTime = cookingTimeInput.value;
-        app.customerArrivalTimeInput = customerArrivalTimeInput.value;
+        simulation.cookingTime = cookingTimeInput.value;
+        simulation.customerArrivalTime = customerArrivalTimeInput.value;
 
+        simulation.start();
         startStopButton.textContent = "Stop";
     }
 }
